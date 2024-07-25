d149cee09278d0c2a9cf91ebb92bc03e3240c44f
ZanderGous <zander.m.gous@gmail.com>
2024-07-25 21:29:59 +0200

    [#6] Implemented greeter function with name argument

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,14 @@
 public class Greeter {
     public static void main(String[] args) {
-        // Empty main function
+        if (args.length > 0) {
+            greeter(args[0]);
+        } else {
+            System.out.println("No name provided! Please provide a name as a command-line argument.");
+        }
+    }
+
+    public static void greeter(String name) {
+        System.out.println("Hello, " + name + "! Welcome to our wonderful community. We're thrilled to have you here. " +
+                           "Feel free to explore and let us know if you need any assistance. Have a fantastic day!");
     }
 }
